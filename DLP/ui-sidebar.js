f40cfe9fbbca6ef91f6c3b7d51d2fd883ba5cf2f
ccofres <43300768+ccofres@users.noreply.github.com>
2020-11-27 08:27:37 -0300

    Listo: Intro a GAN En desarrollo: todos los articulos de más.
    -Sidebar ahora se inserta al codigo en HTML desde ui-sidebar.js
    -styles.css ==> estilos generales
    -papers.css ==> estilos para papers.html/papers.js
    -art-styles ==> estilos para la descripción de los articulos
                    Modificar para mejorar la visualización

--- DLP/ui-sidebar.js
+++ DLP/ui-sidebar.js
@@ -1,16 +1,77 @@
-function run() {
-  let d1 = document.getElementById("mySidenav");
-  d1.insertAdjacentHTML(
-    "afterbegin",
-    '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><a href="./modelos/model-1/model-1.html">Model 1</a><a href="./modelos/model-2/model-2.html">Model 2</a><a href="./modelos/model-3/model-3.html">Clasificador Iris</a><a href="./modelos/model-4/model-4.html">Multiclasificador BCWD</a><a href="./modelos/model-5/model-5.html">Clasificador MNIST</a><a href="./modelos/model-6/model-6.html">Clasificador Fashion MNIST</a><a href="./modelos/model-7/model-7.html">Transfer Learning con MobileNet</a><a href="./modelos/model-8/model-8.html">Style Transfer con Magenta.js</a><a href="./modelos/template-model/template-model.html">Template</a>'
-  );
-}
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function DenseNet() {
+  closeNav();
+  let list = document.getElementById("mySidenav");
+  // As long as <ul> has a child node, remove it
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
 
-document.addEventListener("DOMContentLoaded", run);
+  let d1 = document.getElementById("mySidenav");
+  d1.insertAdjacentHTML(
+    "afterbegin",
+    '<a href="javascript:void(0)" class="closebtn removeElement" onclick="closeNav()">&times;</a><a class="removeElement" href="./modelos/model-1/model-1.html" >Model 1</a><a class="removeElement" href="./modelos/model-2/model-2.html">Model 2</a><a class="removeElement" href="./modelos/model-3/model-3.html">Clasificador Iris</a><a class="removeElement" href="./modelos/model-4/model-4.html">Multiclasificador BCWD</a><a href="javascript:void(0)" class="closebtn removeElement" onclick="run()">&laquo;</a>'
+  );
+  time = setTimeout(openNav, 500);
+}
+
+function CnnNet() {
+  closeNav();
+  let list = document.getElementById("mySidenav");
+  // As long as <ul> has a child node, remove it
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+
+  //closeNav();
+  let d1 = document.getElementById("mySidenav");
+  d1.insertAdjacentHTML(
+    "afterbegin",
+    '<a href="javascript:void(0)" class="closebtn removeElement" onclick="closeNav()">&times;</a><a class="removeElement" href="./modelos/model-5/model-5.html">Clasificador MNIST</a><a class="removeElement" href="./modelos/model-6/model-6.html">Clasificador Fashion MNIST</a><a class="removeElement" href="./modelos/model-7/model-7.html">Transfer Learning con MobileNet</a><a class="removeElement" href="./modelos/model-8/model-8.html">Style Transfer con Magenta.js</a><a href="javascript:void(0)" class="closebtn removeElement" onclick="run()">&laquo;</a>'
+  );
+  time = setTimeout(openNav, 500);
+}
+
+function GenNet() {
+  closeNav();
+  let list = document.getElementById("mySidenav");
+  // As long as <ul> has a child node, remove it
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+
+  let d1 = document.getElementById("mySidenav");
+  d1.insertAdjacentHTML(
+    "afterbegin",
+    '<a href="javascript:void(0)" class="closebtn removeElement" onclick="closeNav()">&times;</a><a class="removeElement" href="./modelos/model-8/model-8.html">GAN</a><a href="javascript:void(0)" class="closebtn removeElement" onclick="run()">&laquo;</a>'
+  );
+  time = setTimeout(openNav, 500);
+}
+
+function run() {
+  closeNav();
+  let list = document.getElementById("mySidenav");
+  // As long as <ul> has a child node, remove it
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  let bar = document.getElementById("mySidenav");
+  bar.insertAdjacentHTML(
+    "afterbegin",
+    '<a href="javascript:void(0)" class="closebtn removeElement" onclick="closeNav()">&times;</a><a href="javascript:void(0)" class="removeElement" onclick="DenseNet()">Dense Net</a><a href="javascript:void(0)" class="removeElement" onclick="CnnNet()">Redes Convolucionales</a><a class="removeElement" href="javascript:void(0)" onclick="GenNet()">Generativos</a>'
+  );
+  time = setTimeout(openNav, 500);
+}
+function firstTime() {
+  let bar = document.getElementById("mySidenav");
+  bar.insertAdjacentHTML(
+    "afterbegin",
+    '<a href="javascript:void(0)" class="closebtn removeElement" onclick="closeNav()">&times;</a><a href="javascript:void(0)" class="removeElement" onclick="DenseNet()">Dense Net</a><a href="javascript:void(0)" class="removeElement" onclick="CnnNet()">Redes Convolucionales</a><a class="removeElement" href="javascript:void(0)" onclick="GenNet()">Generativos</a>'
+  );
+}
+document.addEventListener("DOMContentLoaded", firstTime);
